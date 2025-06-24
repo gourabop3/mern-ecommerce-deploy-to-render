ac9ff10be057b801f3a263cbca3387ca2d172acd
gourabop3 <gourabmullick200@gmail.com>
2025-06-24 16:37:38 +0530

    Update product-details.jsx

--- client/src/components/shopping-view/product-details.jsx
+++ client/src/components/shopping-view/product-details.jsx
@@ -40,7 +40,7 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
         const getQuantity = getCartItems[indexOfCurrentItem].quantity;
         if (getQuantity + 1 > getTotalStock) {
           toast({
-            title: `Only ${getQuantity} quantity can be added for this item`,
+            title: `Only ₹{getQuantity} quantity can be added for this item`,
             variant: "destructive",
           });
 
